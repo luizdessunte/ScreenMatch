9742311809a929c95fb8d9ee960dfdcc079e45da
Luiz Dessunte <102454818+luizdessunte@users.noreply.github.com>
2026-04-09 13:30:27 -0300

    Implementar ArrayList de filmes na classe Principal

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -50,5 +52,19 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoLuiz = new Filme();
+        filmeDoLuiz.setDuracaoEmMinutos(182);
+        filmeDoLuiz.setNome("Vingadores");
+        filmeDoLuiz.setAnoDeLancamento(2012);
+        filmeDoLuiz.avalia(10);
+
+        ArrayList<Filme> listaFilmes = new ArrayList<>();
+        listaFilmes.add(filmeDoLuiz);
+        listaFilmes.add(meuFilme);
+        listaFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " +  listaFilmes.size());
+        System.out.println("Primeiro filme " + listaFilmes.get(0).getNome());
+        System.out.println(listaFilmes);
     }
 }
